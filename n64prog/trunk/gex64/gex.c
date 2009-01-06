dfb22ec3c1be43ee916a4d36ba9cf92d8482ed39
zzt256 <zzt256@a1766706-8254-0410-bc9d-13e0cd63e8ea>
2009-01-06 09:36:57 +0000

    Formatted

--- n64prog/trunk/gex64/gex.c
+++ n64prog/trunk/gex64/gex.c
@@ -6,18 +6,18 @@ gexMakeBox(status_box, 180, 40, 10, 10, 0, 0, 0, 255);
 
 int _start ( void )
 {
-	char buffer[0x100];
-	static int j = 0;
-	
-	/* Draw box */
-	gexDrawBox(&status_box);
-	
-	/* Prepare text */
-	snprintf( buffer, sizeof(buffer), "NUMBER %08X\n", j++ );
-	
-	/* Print text */
-	gexDrawText( buffer, 12, 12, 1 );
-	
-	return 0;
+    char buffer[0x100];
+    static int j = 0;
+    
+    /* Draw box */
+    gexDrawBox(&status_box);
+    
+    /* Prepare text */
+    snprintf( buffer, sizeof(buffer), "NUMBER %08X\n", j++ );
+    
+    /* Print text */
+    gexDrawText( buffer, 12, 12, 1 );
+    
+    return 0;
 }
-	
+    
