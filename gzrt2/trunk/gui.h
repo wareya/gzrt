b6102f8b5bdb017a5b8ca6837a57850534a890a7
zzt256 <zzt256@a1766706-8254-0410-bc9d-13e0cd63e8ea>
2009-01-31 00:42:52 +0000

    Compat for old GTK

--- gzrt2/trunk/gui.h
+++ gzrt2/trunk/gui.h
@@ -4,6 +4,13 @@
 #ifndef __GZRT_GUI_H__
 #define __GZRT_GUI_H__
 
+#include <gtk/gtk.h>
+
+/* Replacements for old GTKs */
+#if GTK_MINOR_VERSION < 10
+# define gtk_window_set_deletable
+#endif
+
 /* Constants */
 #define GZRT_GUI_RES_PATH		"./res"
 #define GZRT_GUI_RES_LOGO		GZRT_GUI_RES_PATH "/logo.svg"
