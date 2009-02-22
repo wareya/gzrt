575472dd1b4ae3d66d9afef227f2fd98132df543
zzt256 <zzt256@a1766706-8254-0410-bc9d-13e0cd63e8ea>
2009-02-22 15:24:57 +0000

    CRC fixing added to n64bl & proper GPL header added to n64bl-gui

--- n64utils/trunk/n64bl-gui/n64bl-gui.c
+++ n64utils/trunk/n64bl-gui/n64bl-gui.c
@@ -1,6 +1,23 @@
-/********************************
-* Nintendo 64 Binary Loader GUI *
-********************************/
+/*
+*   Nintendo 64 Binary Loader GTK frontend
+*   Copyright (c) 2009  ZZT32 [mbr@64.vg]
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 2 of the License, or
+*   any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public
+*   Licence along with this program; if not, write to the Free
+*   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+*   Boston, MA  02110-1301, USA
+*/
+
 #include <stdio.h>
 #include <gtk/gtk.h>
 #include <unistd.h>
